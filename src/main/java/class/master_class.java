005b65fcfc4871769c1025cae0c1eb05d72f5c1e
amdadul-haque <amdadul1807107@gmail.com>
2022-05-08 22:43:37 +0600

    class from master branch

---
+++ src/main/java/class/master_class.java
@@ -0,0 +1,14 @@
+public class master_class{
+  int roll;
+
+  master_class(int r){
+  this.roll = r;
+  }
+  public int getRoll(){
+    return roll;
+  }
+  public void setRoll(int roll){
+    this.roll = roll;
+  }
+}
+
